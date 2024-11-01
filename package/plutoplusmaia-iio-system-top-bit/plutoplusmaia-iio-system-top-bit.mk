953dd591d5c4d4b2f5f85d44f5d4314d35265e1d
F5OEO <evaristec@gmail.com>
2024-11-01 11:49:26 +0100

    PLutoplus maia-iio

---
+++ package/plutoplusmaia-iio-system-top-bit/plutoplusmaia-iio-system-top-bit.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# plutoplus-system-top-bit
+#
+################################################################################
+
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_VERSION = v0.38
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_SOURCE = system_top.xsa
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_SITE = $(BR2_EXTERNAL_PLUTOSDR_PATH)/board/plutoplus/bitstream/maia-iio
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_SITE_METHOD = local
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_INSTALL_IMAGES = YES
+PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_INSTALL_TARGET = NO
+
+define PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_INSTALL_IMAGES_CMDS
+	$(UNZIP) -o $(@D)/$(PLUTOPLUSMAIA_IIO_SYSTEM_TOP_BIT_SOURCE) system_top.bit -d $(@D)
+	cp -f $(@D)/system_top.bit $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
