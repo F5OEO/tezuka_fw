25540f37b3fa042b490b2b7e8b864c329ed01e4d
F5OEO <evaristec@gmail.com>
2024-11-27 12:45:33 +0100

    Add Antsdr e200

---
+++ board/e200/post_image-uboot.sh
@@ -0,0 +1,57 @@
+BOARD_DIR=$(dirname $0)
+BIN_DIR=$1
+dfu_suffix=$HOST_DIR/bin/dfu-suffix
+mkimage=$HOST_DIR/bin/mkimage
+
+DEVICE_VID=0x0456
+DEVICE_PID=0xb673
+
+echo "generating the boot.img"
+
+GENIMAGE_CFG="${BOARD_DIR}/genimage-boot.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+    --rootpath "${TARGET_DIR}" \
+    --tmppath "${GENIMAGE_TMP}" \
+    --inputpath "${BINARIES_DIR}" \
+    --outputpath "${BINARIES_DIR}" \
+    --config "${GENIMAGE_CFG}"
+
+echo "generating the boot.frm"
+cat $BIN_DIR/boot.img $BIN_DIR/uboot-env.bin $BOARD_DIR/target_mtd_info.key | \
+	tee $BIN_DIR/boot.frm | md5sum | cut -d ' ' -f1 | tee -a $BIN_DIR/boot.frm
+
+echo "generating boot.dfu"
+cp $BIN_DIR/boot.img $BIN_DIR/boot.bin.tmp
+$dfu_suffix -a $BIN_DIR/boot.bin.tmp -v $DEVICE_VID -p $DEVICE_PID
+mv $BIN_DIR/boot.bin.tmp $BIN_DIR/boot.dfu
+
+echo "generating uboot-env.dfu"
+mkenvimage -s 0x20000 -o $BIN_DIR/uboot-env.bin $BOARD_DIR/uboot-env.txt
+cp $BIN_DIR/uboot-env.bin $BIN_DIR/uboot-env.bin.tmp
+$dfu_suffix -a $BIN_DIR/uboot-env.bin.tmp -v $DEVICE_VID -p $DEVICE_PID
+mv $BIN_DIR/uboot-env.bin.tmp $BIN_DIR/uboot-env.dfu
+
+echo "generatind sd"
+SDIMGDIR=$BIN_DIR/sdimg
+mkdir -p $SDIMGDIR
+touch 	$SDIMGDIR/boot.bif
+cp $BIN_DIR/u-boot $SDIMGDIR/u-boot.elf
+cp $BIN_DIR/system_top.bit $SDIMGDIR/
+#SHOULD BE WARNING WHEN BUILDING COMPLETE FPGA
+cp $BOARD_DIR/bitstream/fsbl.elf $BIN_DIR
+cp $BIN_DIR/fsbl.elf $SDIMGDIR/
+echo "img : {[bootloader] $SDIMGDIR/fsbl.elf  $SDIMGDIR/system_top.bit  $SDIMGDIR/u-boot.elf}" >  $SDIMGDIR/boot.bif
+bootgen -image $SDIMGDIR/boot.bif -w -o i $SDIMGDIR/BOOT.bin
+rm $SDIMGDIR/fsbl.elf  $SDIMGDIR/system_top.bit  $SDIMGDIR/u-boot.elf $SDIMGDIR/boot.bif
+cp $BIN_DIR/rootfs.cpio.gz $SDIMGDIR/ramdisk.image.gz
+$mkimage -A arm -T ramdisk -C gzip -d $SDIMGDIR/ramdisk.image.gz $SDIMGDIR/uramdisk.image.gz
+rm $SDIMGDIR/ramdisk.image.gz
+cp $BIN_DIR/pluto.dfu $SDIMGDIR/uImage
+cp $BIN_DIR/zynq-e200.dtb $SDIMGDIR/devicetree.dtb
+cp $BOARD_DIR/uboot-env.txt $SDIMGDIR/
+
+cd $BIN_DIR && zip tezuka.zip boot.dfu boot.frm pluto.frm pluto.dfu sdimg/*
