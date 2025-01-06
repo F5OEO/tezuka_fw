9c2db8839c4c6ddc1702f0980b752492b603e787
F5OEO <evaristec@gmail.com>
2025-01-06 16:36:02 +0100

    Add tools

---
+++ tools/jtag-recovery/recover_xvc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+openocd -f plutoxvc.cfg &
+sleep 2
+{ 
+    echo "targets zynq.cpu0";
+    echo "halt";
+echo "ps7_debug";
+echo "ps7_init";
+sleep 10
+echo "ps7_post_config";
+echo "poll on"
+echo "load_image u-boot.elf"
+sleep 15
+echo "resume 0x04000000"
+echo "exit"
+} | telnet localhost 4444
