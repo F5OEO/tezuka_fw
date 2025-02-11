fea04684bdabd78e88eeebac674a98ce2ca31aa5
Dawid SQ6EMM <sq6emm@hamradio.pl>
2025-02-11 22:11:16 +0100

    fix to propery display values
    Signed-off-by: Dawid SQ6EMM <sq6emm@hamradio.pl>
    Signed-off-by: pa3gsb <pa3gsb@gmail.com>

--- board/pluto/overlay_maia/root/mqtt_status.sh
+++ board/pluto/overlay_maia/root/mqtt_status.sh
@@ -3,13 +3,14 @@
 
 prefix='/sys/bus/iio/devices/iio:device0/'
 
-sources="out_voltage0_rf_port_select in_voltage0_rf_port_select out_altvoltage1_TX_LO_frequency out_altvoltage0_RX_LO_frequency out_voltage_rf_bandwidth in_voltage_rf_bandwidth xo_correction out_voltage_sampling_frequency in_voltage_sampling_frequency in_voltage0_gain_control_mode in_voltage0_hardwaregain out_voltage0_hardwaregain"
+sources="out_voltage0_rf_port_select in_voltage0_rf_port_select out_altvoltage1_TX_LO_frequency out_altvoltage0_RX_LO_frequency out_voltage_rf_bandwidth in_voltage_rf_bandwidth xo_correction out_voltage_sampling_frequency in_voltage_sampling_frequency in_voltage0_gain_control_mode in_voltage0_hardwaregain out_voltage0_hardwaregain out_altvoltage1_TX_LO_powerdown out_altvoltage0_RX_LO_powerdown"
 
 while true; do
 
   for i in $sources; do
 
-    /usr/bin/mosquitto_pub -r -i "tezuka_pub" -t "status/$i" -s < $prefix$i
+    val="`cat $prefix$i`"
+    /usr/bin/mosquitto_pub -r -i "tezuka_pub" -t "status/$i" -m "$val"
 
   done
   
