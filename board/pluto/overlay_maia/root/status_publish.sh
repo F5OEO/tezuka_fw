186a11f4606cba30c2b7440cfc7ef70f2f1b9b1a
Dawid SQ6EMM <sq6emm@hamradio.pl>
2025-02-14 23:04:19 +0100

    add capabilities reporting / possible values + new info
    Signed-off-by: Dawid SQ6EMM <sq6emm@hamradio.pl>
    Signed-off-by: pa3gsb <pa3gsb@gmail.com>

--- board/pluto/overlay_maia/root/status_publish.sh
+++ board/pluto/overlay_maia/root/status_publish.sh
@@ -4,6 +4,7 @@
 
 folder='/sys/bus/iio/devices/iio:device0/'
 
+# List of current values for different settings
 declare -A VMAP=(
   [${folder}out_voltage0_rf_port_select]='tx/ant'
   [${folder}in_voltage0_rf_port_select]='rx/ant'
@@ -21,46 +22,68 @@ declare -A VMAP=(
   [${folder}out_altvoltage0_RX_LO_powerdown]='rx/active'
 )
 
-# based on VMAP, declare rVMAP
+# List of possible values (capabilities/caps) for reported values settings
+declare -A cVMAP=(
+  [${folder}out_voltage_rf_port_select_available]='caps/tx/ant'
+  [${folder}in_voltage_rf_port_select_available]='caps/rx/ant'
+  [${folder}out_altvoltage1_TX_LO_frequency_available]='caps/tx/frequency'
+  [${folder}out_altvoltage0_RX_LO_frequency_available]='caps/rx/frequency'
+  [${folder}out_voltage_rf_bandwidth_available]='caps/tx/bandwidth'
+  [${folder}in_voltage_rf_bandwidth_available]='caps/rx/bandwidth'
+  [${folder}xo_correction_available]='caps/main/freq_correction'
+  [${folder}out_voltage_sampling_frequency_available]='caps/tx/sampling'
+  [${folder}in_voltage_sampling_frequency_available]='caps/rx/sampling'
+  [${folder}in_voltage_gain_control_mode_available]='caps/rx/gain_mode'
+  [${folder}in_voltage0_hardwaregain_available]='caps/rx/gain'
+  [${folder}out_voltage0_hardwaregain_available]='caps/tx/gain'
+)
 
+# based on VMAP, declare rVMAP
 declare -A rVMAP
 for K in "${!VMAP[@]}"; do rVMAP[${VMAP[$K]}]=$K; done
 
-# Functions to handle the data
-
+# Publish to mqtt
 mqtt_publish () {
-  /usr/bin/mosquitto_pub -r -i "tezuka_pub" -t "dt/$1" -m "$2"
+  /usr/bin/mosquitto_pub -r -i "tezuka_pub" -t "state/${1}" -m "${2}"
 }
 
+# Publish to serial port if needed - currently to stdout for DEBUG only
 serial_publish () {
   # for future use to publish to a serial port
-  echo $1 $2
+  echo ${1} ${2}
 }
 
+# Generic publish function
 publish () {
-  mqtt_publish $1 $2
-  serial_publish $1 $2
+  mqtt_publish "${1}" "${2}"
+  serial_publish "${1}" "${2}"
 }
 
+# File reader with error handling
 read_file () {
   if [ -r "$1" ]; then
-    cat "$1"
+    cat "${1}"
   else
     echo 'n/a'
   fi
 }
 
+# Publish data
 update_data () {
   while read i; do
     k=${VMAP[$i]};
     v="`read_file $i`";
-    publish $k $v;
+    publish ${k} ${v};
   done
 }
 
+# Collect all settings we want to report
 dump_data () {
   for K in "${!VMAP[@]}"; do publish ${VMAP[$K]} "`read_file $K`"; done
+  for K in "${!cVMAP[@]}"; do publish ${cVMAP[$K]} "`read_file $K`"; done
   publish "main/serial" "`read_file /etc/serial`"
+  publish "main/hw_model" "`grep 'hw_model=' /etc/libiio.ini | sed s,hw_model=,,`"
+  publish "main/fw_version" "`grep 'fw_version=' /etc/libiio.ini | sed s,fw_version=,,`"
 }
 
 
