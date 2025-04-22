3bb7e7dc7f95d31dcbb8dc2eacf40734f6b7ebe5
Alexander Voronov <kab00m@ya.ru>
2025-04-22 13:46:53 +0300

    Enahnce getbuildroot.sh
    Signed-off-by: Alexander Voronov <kab00m@ya.ru>

--- getbuildroot.sh
+++ getbuildroot.sh
@@ -1,1 +1,2 @@
-mkdir -p buildroot && wget -O- https://buildroot.org/downloads/buildroot-2024.11.1.tar.gz | tar -xz -C buildroot --strip-component 1
+#!/usr/bin/env bash
+wget -O- https://buildroot.org/downloads/buildroot-2024.11.1.tar.gz | tar -xz --one-top-level=buildroot --strip-components=1
