eca04ca6f7ad42000880e655e69af49489e0b03d
F5OEO <evaristec@gmail.com>
2025-04-24 14:01:20 +0200

    Add fishball 7020 board

---
+++ package/fishball_fpga_7020/fishball_fpga.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# fishball-system-top-bit
+#
+################################################################################
+
+FISHBALL_FPGA_7020_VERSION = v0.38
+FISHBALL_FPGA_7020_SOURCE =system_top.xsa
+FISHBALL_FPGA_7020_SITE = $(BR2_EXTERNAL_PLUTOSDR_PATH)/board/fishball_7020/bitstream/maia-iio
+FISHBALL_FPGA_7020_SITE_METHOD = local
+FISHBALL_FPGA_7020_INSTALL_IMAGES = YES
+FISHBALL_FPGA_7020_INSTALL_TARGET = NO
+
+define FISHBALL_FPGA_7020_INSTALL_IMAGES_CMDS
+	$(UNZIP) -o $(@D)/$(FISHBALL_FPGA_7020_SOURCE) system_top.bit -d $(@D)
+	cp -f $(@D)/system_top.bit $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
