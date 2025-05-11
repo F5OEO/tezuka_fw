3588f2b66e85c302d120a0c0efc3162ab2bf75aa
xwings <code@emulate.sh>
2025-05-11 21:39:16 +0800

    more signalsdrpro config

--- board/signalsdrpro/post_image-uboot.sh
+++ board/signalsdrpro/post_image-uboot.sh
@@ -49,4 +49,4 @@ mkimage -A arm -O linux -T kernel -C none -a 0x2080000 -e 2080000 -n "Linux kern
 cp $BIN_DIR/zynq-signalsdrpro.dtb $SDIMGDIR/devicetree.dtb
 cp $BOARD_DIR/uboot-env.txt $SDIMGDIR/uEnv.txt
 
-cd $BIN_DIR && zip tezuka.zip boot.dfu boot.frm pluto.frm pluto.dfu sdimg/*
+cd $BIN_DIR && zip tezuka.zip boot.dfu boot.frm signalsdrpro.frm signalsdrpro.dfu sdimg/*
--- board/signalsdrpro/post_image.sh
+++ board/signalsdrpro/post_image.sh
@@ -7,17 +7,17 @@ dfu_suffix=$HOST_DIR/bin/dfu-suffix
 DEVICE_VID=0x0456
 DEVICE_PID=0xb673
 
-cp $BOARD_DIR/pluto.its $BIN_DIR/pluto.its
+cp $BOARD_DIR/signalsdrpro.its $BIN_DIR/signalsdrpro.its
 
 echo "# entering $BIN_DIR for the next command"
-(cd $BIN_DIR && $mkimage -f pluto.its pluto.itb)
+(cd $BIN_DIR && $mkimage -f signalsdrpro.its signalsdrpro.itb)
 
 echo "generating the pluto.frm"
-md5sum $BIN_DIR/pluto.itb | cut -d ' ' -f 1 > $BIN_DIR/pluto.md5
-cat $BIN_DIR/pluto.itb  $BIN_DIR/pluto.md5 > $BIN_DIR/pluto.frm
+md5sum $BIN_DIR/signalsdrpro.itb | cut -d ' ' -f 1 > $BIN_DIR/signalsdrpro.md5
+cat $BIN_DIR/signalsdrpro.itb  $BIN_DIR/pluto.md5 > $BIN_DIR/signalsdrpro.frm
 
 echo "generating pluto.dfu"
 $dfu_suffix -a $BIN_DIR/pluto.itb -v $DEVICE_VID -p $DEVICE_PID
-mv $BIN_DIR/pluto.itb $BIN_DIR/pluto.dfu
+mv $BIN_DIR/signalsdrpro.itb $BIN_DIR/signalsdrpro.dfu
 
-rm -f $BIN_DIR/pluto.its $BIN_DIR/*.md5
+rm -f $BIN_DIR/signalsdrpro.its $BIN_DIR/*.md5
