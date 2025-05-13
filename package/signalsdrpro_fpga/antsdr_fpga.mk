0e9b97023a1c10252470d762e8353e70a44567a8
xwings <kj@xandora.net>
2025-05-14 07:44:14 +0800

    add signalsdrpro_fpga

---
+++ package/signalsdrpro_fpga/antsdr_fpga.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# plutoplus-system-top-bit
+#
+################################################################################
+
+SIGNALSDRPRO_FPGA_VERSION = v0.38
+SIGNALSDRPRO_FPGA_SOURCE =system_top.xsa
+SIGNALSDRPRO_FPGA_SITE = $(BR2_EXTERNAL_SIGNALSDRPRO_PATH)/board/signalsdrpro/bitstream/maia-iio
+SIGNALSDRPRO_FPGA_SITE_METHOD = local
+SIGNALSDRPRO_FPGA_INSTALL_IMAGES = YES
+SIGNALSDRPRO_FPGA_INSTALL_TARGET = NO
+
+define SIGNALSDRPRO_FPGA_INSTALL_IMAGES_CMDS
+	$(UNZIP) -o $(@D)/$(SIGNALSDRPRO_FPGA_SOURCE) system_top.bit -d $(@D)
+	cp -f $(@D)/system_top.bit $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
