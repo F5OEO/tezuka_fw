885b3772db7f453d8347dbc65f1b4f597b9ab23b
xwings <kj@xandora.net>
2025-05-13 22:31:55 +0800

    update dts file

--- board/signalsdrpro/post_image.sh
+++ board/signalsdrpro/post_image.sh
@@ -1,4 +1,4 @@
-TARGET=pluto
+TARGET=signalsdrpro
 BOARD_DIR=$(dirname $0)
 BIN_DIR=$1
 mkimage=$HOST_DIR/bin/mkimage
@@ -12,12 +12,12 @@ cp $BOARD_DIR/signalsdrpro.its $BIN_DIR/signalsdrpro.its
 echo "# entering $BIN_DIR for the next command"
 (cd $BIN_DIR && $mkimage -f signalsdrpro.its signalsdrpro.itb)
 
-echo "generating the pluto.frm"
+echo "generating the signalsdrpro.frm"
 md5sum $BIN_DIR/signalsdrpro.itb | cut -d ' ' -f 1 > $BIN_DIR/signalsdrpro.md5
-cat $BIN_DIR/signalsdrpro.itb  $BIN_DIR/pluto.md5 > $BIN_DIR/signalsdrpro.frm
+cat $BIN_DIR/signalsdrpro.itb  $BIN_DIR/signalsdrpro.md5 > $BIN_DIR/signalsdrpro.frm
 
-echo "generating pluto.dfu"
-$dfu_suffix -a $BIN_DIR/pluto.itb -v $DEVICE_VID -p $DEVICE_PID
+echo "generating signalsdrpro.dfu"
+$dfu_suffix -a $BIN_DIR/signalsdrpro.itb -v $DEVICE_VID -p $DEVICE_PID
 mv $BIN_DIR/signalsdrpro.itb $BIN_DIR/signalsdrpro.dfu
 
 rm -f $BIN_DIR/signalsdrpro.its $BIN_DIR/*.md5
