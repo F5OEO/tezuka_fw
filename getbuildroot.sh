a0385113a8eb06e365c73dd48d64ca7812966b7f
F5OEO <evaristec@gmail.com>
2025-05-26 16:53:51 +0200

    Update buildroot to 2025.02.3

--- getbuildroot.sh
+++ getbuildroot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wget -O- https://buildroot.org/downloads/buildroot-2024.11.1.tar.gz | tar -xz --one-top-level=buildroot --strip-components=1
+wget -O- https://buildroot.org/downloads/buildroot-2025.02.3.tar.gz | tar -xz --one-top-level=buildroot --strip-components=1
